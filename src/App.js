01f8318dc603b9b3e57f552ee62df2f05c4e3e58
ArchwayJay <111459103+ArchwayJay@users.noreply.github.com>
2023-08-25 16:06:24 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Greeter from "./Greeter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Greeter />
+    </div>
+  );
+}
---
+++ src/Greeter.js
@@ -0,0 +1,8 @@
+export default function Greeter() {
+  return (
+    <>
+      <p>Helloe</p>
+      <button>Check</button>
+    </>
+  );
+}
